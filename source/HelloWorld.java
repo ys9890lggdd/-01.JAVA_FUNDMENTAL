39fcb366aaad0465d08476e00e747c25975a423e
academyitwill <academyitwill@gmail.com>
2022-11-08 17:32:32 +0900

    no message

---
+++ source/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main(String[] args){
+ 		// 문자열 출력 명령문(주석)
+		System.out.println("Hello Java!");
+ 		System.out.println("안녕 자바1!");
+		System.out.println("안녕 자바2!");
+	}
+}
